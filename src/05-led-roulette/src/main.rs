df78a9134f5437a2bc714b6fcbc384578702018a
Ryan Fickenscher <rsficken@gmail.com>
2021-09-19 09:41:12 -0700

    led roulette

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -9,14 +9,18 @@ use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 fn main() -> ! {
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    let mut half_period = 500_u16;
-    let v_half_period = Volatile::new(&mut half_period);
+    let mut ms = 50_u8;
+    let v_ms = Volatile::new(&mut ms);
 
     loop {
-        leds[0].on().ok();
-        delay.delay_ms(v_half_period.read());
+        for curr in 0..8 {
+            let next = (curr + 1) % 8;
 
-        leds[0].off().ok();
-        delay.delay_ms(v_half_period.read());
+            leds[next].on().ok();
+            delay.delay_ms(v_ms.read());
+
+            leds[curr].off().ok();
+            delay.delay_ms(v_ms.read());
+        }
     }
 }
